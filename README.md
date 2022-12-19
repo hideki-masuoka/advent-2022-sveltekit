1b7940aa277d94210b0d28810a64e1fb277df788
hansendo <hansendo@localhost.localdomain>
2022-12-19 23:07:12 +0900

    init sample project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# advent-2022-sveltekit
+# くお〜!!Unused CSS selector〜!!ここでonwarn全開、インド人を=>に！
+
+この記事は[SvelteKitのカレンダー | Advent Calendar 2022 - Qiita](https://qiita.com/advent-calendar/2022/sveltekit)の20日目となる投稿です。
+
+movie
+
+## 
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,63 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<div class="no-im-used">くお〜!!</div>
+		<hr />
+		<div class="yes-im-used btn">(^o^)</div>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style lang="scss">
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+
+		.no-im-used {
+			@apply text-4xl btn;
+		}
+		.yes-im-used {
+			@apply text-4xl;
+		}
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
